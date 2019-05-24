b54324549292316f2495e000aecae4617e4312a7
Gourisha Hegde <gourishdhegde@gmail.com>
2019-05-24 22:04:44 +0530

    Single inheritance example completed

--- NPTEL_Java_Course/src/inheritancePractice/SingleInheritance.java
+++ NPTEL_Java_Course/src/inheritancePractice/SingleInheritance.java
@@ -1,9 +1,55 @@
 package inheritancePractice;
 
+class square {
+	double x , y;
+	
+	square(){
+		this.x = 0.5;
+		this.y = 0.5;
+	}
+	
+	square(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void area(){
+		System.out.println("Area of Square is "+x*y);
+	}
+}
+
+class volume extends square {
+	double z;
+	
+	public volume() {
+		super();
+		this.z = 0.5;
+	}
+
+	public volume(double x, double y, double z) {
+		super(x,y);
+		this.z = z;
+	}
+	
+	public void area(){
+		System.out.println("Volume of Cube is "+x*y*z);
+	}
+}
 public class SingleInheritance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		square s1 = new square();
+		s1.area();
+		
+		square s2 = new square(2.5, 3.5);
+		s2.area();
+		
+		volume v1 = new volume();
+		v1.area();
+		
+		volume v2 = new volume(3.5, 4.5, 5.5);
+		v2.area();
 
 	}
 
